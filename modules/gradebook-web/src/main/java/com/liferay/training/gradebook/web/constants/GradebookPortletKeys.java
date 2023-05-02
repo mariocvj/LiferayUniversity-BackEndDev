8df51b56d20f3d2750d933b486a6a013f039ed8a
Mario Cvjetojević <mario.025025@gmail.com>
2023-05-02 14:50:23 +0200

    created gradebook-web module

---
+++ modules/gradebook-web/src/main/java/com/liferay/training/gradebook/web/constants/GradebookPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.training.gradebook.web.constants;
+
+/**
+ * @author user
+ */
+public class GradebookPortletKeys {
+
+	public static final String GRADEBOOK =
+		"com_liferay_training_gradebook_web_GradebookPortlet";
+
+}
