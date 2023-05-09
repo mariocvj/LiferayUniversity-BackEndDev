f19668f07130c39ab7e696e2882f1247f12d8d5c
Mario Cvjetojević <mario.025025@gmail.com>
2023-05-09 11:29:14 +0200

    service module permissions implemented

---
+++ modules/gradebook/gradebook-api/src/main/java/com/liferay/training/gradebook/constants/GradebookConstants.java
@@ -0,0 +1,8 @@
+package com.liferay.training.gradebook.constants;
+
+/**
+ * @author Mario Cvjetojevic
+ */
+public class GradebookConstants {
+    public static final String RESOURCE_NAME = "com.liferay.training.gradebook.model";
+}
--- modules/gradebook/gradebook-service/src/main/java/com/liferay/training/gradebook/service/impl/AssignmentServiceImpl.java
+++ modules/gradebook/gradebook-service/src/main/java/com/liferay/training/gradebook/service/impl/AssignmentServiceImpl.java
@@ -16,8 +16,13 @@ package com.liferay.training.gradebook.service.impl;
 
 import com.liferay.portal.aop.AopService;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionFactory;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.training.gradebook.constants.GradebookConstants;
 import com.liferay.training.gradebook.model.Assignment;
 import com.liferay.training.gradebook.service.base.AssignmentServiceBaseImpl;
 
@@ -27,6 +32,9 @@ import java.util.Locale;
 import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * The implementation of the assignment remote service.
@@ -42,42 +50,58 @@ import org.osgi.service.component.annotations.Component;
  * @see AssignmentServiceBaseImpl
  */
 @Component(
-		property = {
-				"json.web.service.context.name=gradebook",
-				"json.web.service.context.path=Assignment"
-		},
-		service = AopService.class
+	property = {
+		"json.web.service.context.name=gradebook",
+		"json.web.service.context.path=Assignment"
+	},
+	service = AopService.class
 )
 public class AssignmentServiceImpl extends AssignmentServiceBaseImpl {
 
 	/*
 	 * NOTE FOR DEVELOPERS:
-	 *
+ 	 *
 	 * Never reference this class directly. Always use <code>com.liferay.training.gradebook.service.AssignmentServiceUtil</code> to access the assignment remote service.
 	 */
 	public Assignment addAssignment(
-			long groupId, Map<Locale, String> titleMap, String description,
-			Date dueDate, ServiceContext serviceContext)
-			throws PortalException {
+		long groupId, Map<Locale, String> titleMap, String description,
+		Date dueDate, ServiceContext serviceContext)
+		throws PortalException {
+		
+		// Check permissions.
+		
+		_portletResourcePermission.check(
+			getPermissionChecker(), serviceContext.getScopeGroupId(),
+			ActionKeys.ADD_ENTRY);
 
 		return assignmentLocalService.addAssignment(
-				groupId, titleMap, description, dueDate, serviceContext);
+			groupId, titleMap, description, dueDate, serviceContext);
 	}
 
 	public Assignment deleteAssignment(long assignmentId)
-			throws PortalException {
+		throws PortalException {
+		
+		// Check permissions.
+		
+		_assignmentModelResourcePermission.check(
+			getPermissionChecker(), assignmentId, ActionKeys.DELETE);
 
 		Assignment assignment =
-				assignmentLocalService.getAssignment(assignmentId);
+			assignmentLocalService.getAssignment(assignmentId);
 
 		return assignmentLocalService.deleteAssignment(assignment);
 	}
 
 	public Assignment getAssignment(long assignmentId)
-			throws PortalException {
+		throws PortalException {
 
 		Assignment assignment =
-				assignmentLocalService.getAssignment(assignmentId);
+			assignmentLocalService.getAssignment(assignmentId);
+		
+		// Check permissions.
+
+		_assignmentModelResourcePermission.check(
+			getPermissionChecker(), assignment, ActionKeys.VIEW);
 
 		return assignment;
 	}
@@ -88,25 +112,45 @@ public class AssignmentServiceImpl extends AssignmentServiceBaseImpl {
 	}
 
 	public List<Assignment> getAssignmentsByKeywords(
-			long groupId, String keywords, int start, int end,
-			OrderByComparator<Assignment> orderByComparator) {
+		long groupId, String keywords, int start, int end,
+		OrderByComparator<Assignment> orderByComparator) {
 
 		return assignmentLocalService.getAssignmentsByKeywords(
-				groupId, keywords, start, end, orderByComparator);
+			groupId, keywords, start, end, orderByComparator);
 	}
 
 	public long getAssignmentsCountByKeywords(long groupId, String keywords) {
 
 		return assignmentLocalService.getAssignmentsCountByKeywords(
-				groupId, keywords);
+			groupId, keywords);
 	}
 
 	public Assignment updateAssignment(
-			long assignmentId, Map<Locale, String> titleMap, String description,
-			Date dueDate, ServiceContext serviceContext)
-			throws PortalException {
+		long assignmentId, Map<Locale, String> titleMap, String description,
+		Date dueDate, ServiceContext serviceContext)
+		throws PortalException {
+		
+		// Check permissions.
+
+		_assignmentModelResourcePermission.check(
+			getPermissionChecker(), assignmentId, ActionKeys.UPDATE);
 
 		return assignmentLocalService.updateAssignment(
-				assignmentId, titleMap, description, dueDate, serviceContext);
-	}
+			assignmentId, titleMap, description, dueDate, serviceContext);
+	}	
+	@Reference(
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY,
+		target = "(model.class.name=com.liferay.training.gradebook.model.Assignment)"
+	)
+	private volatile ModelResourcePermission<Assignment>
+		_assignmentModelResourcePermission;
+		
+	@Reference(
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY,
+		target = "(resource.name=" + GradebookConstants.RESOURCE_NAME + ")"
+	)
+	private volatile PortletResourcePermission _portletResourcePermission;	
+
 }
